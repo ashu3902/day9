f4d031f62272c1dc832e46d84d5babfd4b2318b2
ashu3902 <moreashutosh1997@gmail.com>
2020-10-24 11:49:29 +0530

    Added: daily wage

--- emp.sh
+++ emp.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -x
 echo "Welcome to Employee wage computation problem"
 isPresent=1
+wageRate=20
+dayHr=8
 randomCheck=$(( RANDOM%2 ))
 if [[ $isPresent -eq $randomCheck ]]
 then
@@ -8,3 +10,5 @@ then
 else
      echo "Employee is not present"
 fi
+dailyWage=$(( $wageRate * $dayHr ))
+echo "Daily employee wage = $dailyWage"
